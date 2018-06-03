c36c41989067bf3224647271bc2043910238bd9d
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-06-03 13:28:09 +0400

    invertCase.js

---
+++ invertCase.js
@@ -0,0 +1,11 @@
+const invertCase = (str) => {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const upper = str[i] === str[i].toUpperCase();
+    result += upper ? str[i].toLowerCase() : str[i].toUpperCase();
+  }
+
+  return result;
+};
+
+console.log(invertCase('Hello, World!'));
