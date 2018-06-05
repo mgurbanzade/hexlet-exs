88b36906b1412dd8bebae89c70e23e5b95cccc68
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-06-05 10:24:25 +0400

    isPalindrome.js

---
+++ Functions/isPalindrome.js
@@ -0,0 +1,13 @@
+const substr = (str, start, size) => str.substr(start, size);
+
+const isPalindrome = (str) => {
+  str = str.toLowerCase();
+  if (str.length <= 2) return true;
+  if (str[0] !== str[str.length-1]) return false;
+
+  // console.log(substr(str, 0+1, (str.length-1) -2))
+
+  return isPalindrome(substr(str, 1, (str.length-1) -1)) ? true : false;
+};
+
+console.log(isPalindrome('aibohphobia'))
