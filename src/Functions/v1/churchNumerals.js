d2055b3b55ebff7b5d1ac7b7604c6abc4edbe7c9
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-06-23 17:22:26 +0400

    church 2

--- src/Functions/v1/churchNumerals.js
+++ src/Functions/v1/churchNumerals.js
@@ -1,5 +1,5 @@
-Zero = (s) => (z) => z;
-Succ = (n) => (s => z => s (n (s) (z)));
+Zero = s => z => z;
+succ = num => fn => x => fn(num(fn)(x))
 
 const two = Succ(Succ(Zero));
 console.log(two(x => x+1)(0))
